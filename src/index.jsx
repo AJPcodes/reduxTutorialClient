3f8d382ef506eb40119055bf89f0ba0731fd423c
Allen Phelps <aphelps@aspirehealthcare.com>
2016-05-12 00:03:44 -0500

    create separate vote and winner display components

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['A Wild Sheep Chase', 'Wind, Pinball'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} winner="A Wild Sheep Chase" />,
   document.getElementById('app')
 );
